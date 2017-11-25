76f3a81f080acf7fc6f41191f0fba720c247a9d6
Himalay <contact@himalay.com.np>
2017-11-25 21:56:34 +0545

    Add url list with menu, settings and url dialogs

--- js/app.js
+++ js/app.js
@@ -1,1 +1,47 @@
-console.log('hi there!')
+(function () {
+  const $ = selector => document.querySelector(selector)
+
+  const $settingsBtn = $('#settingsBtn')
+  const $settingsDialog = $('#settingsDialog')
+  const $settingsForm = $('#settingsForm')
+  const $saveSettingsBtn = $('#saveSettingsBtn')
+  const $cancelSettingsBtn = $('#cancelSettingsBtn')
+
+  const $addBookmarkBtn = $('#addBookmarkBtn')
+  const $bookmarkDialog = $('#bookmarkDialog')
+  const $bookmarkForm = $('#bookmarkForm')
+  const $saveBookmarkBtn = $('#saveBookmarkBtn')
+  const $cancelBookmarkBtn = $('#cancelBookmarkBtn')
+
+  $settingsBtn.addEventListener('click', () => $settingsDialog.showModal())
+  $settingsForm.addEventListener('submit', saveSettings)
+  $saveSettingsBtn.addEventListener('click', saveSettings)
+  $cancelSettingsBtn.addEventListener('click', hideSettingsDialog)
+
+  $addBookmarkBtn.addEventListener('click', () => $bookmarkDialog.showModal())
+  $bookmarkForm.addEventListener('submit', saveBookmark)
+  $saveBookmarkBtn.addEventListener('click', saveBookmark)
+  $cancelBookmarkBtn.addEventListener('click', hideBookmarkDialog)
+
+  function saveSettings () {
+    if ($settingsForm.checkValidity()) {
+      hideSettingsDialog()
+    }
+  }
+
+  function hideSettingsDialog () {
+    $settingsDialog.close()
+    $settingsForm.reset()
+  }
+
+  function saveBookmark () {
+    if ($bookmarkForm.checkValidity()) {
+      hideBookmarkDialog()
+    }
+  }
+
+  function hideBookmarkDialog () {
+    $bookmarkDialog.close()
+    $bookmarkForm.reset()
+  }
+})()
