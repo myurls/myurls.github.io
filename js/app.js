132f5e811d1e5291ae14a4f2040b97ed6a9e9590
Himalay <contact@himalay.com.np>
2017-11-25 22:16:11 +0545

    Save and get access token from localStorage, replace bookmarks with url

--- js/app.js
+++ js/app.js
@@ -1,47 +1,62 @@
 (function () {
   const $ = selector => document.querySelector(selector)
 
+  const tokenKey = 'token'
+  const token = localStorage.getItem(tokenKey)
+  const myURLsKey = 'myURLs'
+  const myURLs = localStorage.getItem(myURLsKey)
+
   const $settingsBtn = $('#settingsBtn')
   const $settingsDialog = $('#settingsDialog')
   const $settingsForm = $('#settingsForm')
   const $saveSettingsBtn = $('#saveSettingsBtn')
   const $cancelSettingsBtn = $('#cancelSettingsBtn')
 
-  const $addBookmarkBtn = $('#addBookmarkBtn')
-  const $bookmarkDialog = $('#bookmarkDialog')
-  const $bookmarkForm = $('#bookmarkForm')
-  const $saveBookmarkBtn = $('#saveBookmarkBtn')
-  const $cancelBookmarkBtn = $('#cancelBookmarkBtn')
+  const $addURLBtn = $('#addURLBtn')
+  const $urlDialog = $('#urlDialog')
+  const $addEdit = $('#addEdit')
+  const $urlForm = $('#urlForm')
+  const $tokenInput = $('#tokenInput')
+  const $saveURLBtn = $('#saveURLBtn')
+  const $cancelURLBtn = $('#cancelURLBtn')
+
+  init()
 
   $settingsBtn.addEventListener('click', () => $settingsDialog.showModal())
   $settingsForm.addEventListener('submit', saveSettings)
   $saveSettingsBtn.addEventListener('click', saveSettings)
   $cancelSettingsBtn.addEventListener('click', hideSettingsDialog)
 
-  $addBookmarkBtn.addEventListener('click', () => $bookmarkDialog.showModal())
-  $bookmarkForm.addEventListener('submit', saveBookmark)
-  $saveBookmarkBtn.addEventListener('click', saveBookmark)
-  $cancelBookmarkBtn.addEventListener('click', hideBookmarkDialog)
+  $addURLBtn.addEventListener('click', () => $urlDialog.showModal())
+  $urlForm.addEventListener('submit', saveURL)
+  $saveURLBtn.addEventListener('click', saveURL)
+  $cancelURLBtn.addEventListener('click', hideURLDialog)
+
+  function init () {
+    $tokenInput.value = token
+  }
 
   function saveSettings () {
     if ($settingsForm.checkValidity()) {
+      localStorage.setItem(tokenKey, $tokenInput.value)
       hideSettingsDialog()
     }
   }
 
   function hideSettingsDialog () {
     $settingsDialog.close()
-    $settingsForm.reset()
   }
 
-  function saveBookmark () {
-    if ($bookmarkForm.checkValidity()) {
-      hideBookmarkDialog()
+  function saveURL () {
+    if ($urlForm.checkValidity()) {
+
+      localStorage.setItem(myURLsKey, $tokenInput.value)
+      hideURLDialog()
     }
   }
 
-  function hideBookmarkDialog () {
-    $bookmarkDialog.close()
-    $bookmarkForm.reset()
+  function hideURLDialog () {
+    $urlDialog.close()
+    $urlForm.reset()
   }
 })()
