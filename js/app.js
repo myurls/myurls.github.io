8bf6688c7280f9dc2867cfa4dbbffd5442eec7b6
Himalay <contact@himalay.com.np>
2017-11-27 17:39:30 +0545

    Remove font icon and use svg instead

--- js/app.js
+++ js/app.js
@@ -274,7 +274,9 @@
       </a>
       <span class="mdl-list__item-secondary-action">
         <button class="android-more-button mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect" id="urlMenu${url.id}" title="Menu">
-          <i class="material-icons">more_vert</i>
+          <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+          <path fill="#424242" d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z" />
+          </svg>
         </button>
         <ul class="mdl-menu mdl-js-menu mdl-menu--bottom-left mdl-js-ripple-effect" for="urlMenu${url.id}" data-id="${url.id}">
           <li data-action="edit" class="mdl-menu__item">Edit</li>
